735918d57a43d81ed67f9464aaaccec92a0480a6
Toan-Nguyen26 <toan.nguyenhai2607@gmail.com>
2024-08-21 08:42:28 +0700

    chore: Update dataset paths in configgenerator.py

--- configgenerator.py
+++ configgenerator.py
@@ -6,11 +6,20 @@ jsondata = {
 #     "word2vecfile": "/home/omri/datasets/word2vec/GoogleNews-vectors-negative300.bin",
 #     "choidataset": "/home/omri/code/text-segmentation-2017/data/choi",
 #     "wikidataset": "/home/omri/datasets/wikipedia/process_dump_r",
+
+       # Ruunning on VSCode
       "word2vecfile": "./datasets/GoogleNews-vectors-negative300.bin",
       "wiki-test-50k": "./datasets/wiki_test_50",
       "wikidataset": "./datasets/wikidataset",
       "half-wikidataset": "./datasets/half-wikidataset",
       "snippets": "./datasets/snippets",
+
+       # Ruunning on VSCode
+      "word2vecfile": "./datasets/my_datasets/GoogleNews-vectors-negative300.bin",
+      "wiki-test-50k": "./datasets/my_datasets/wiki_test_50",
+      "wikidataset": "./datasets/my_datasets/wikidataset",
+      "half-wikidataset": "./datasets/my_datasets/half-wikidataset",
+      "snippets": "./datasets/my_datasets/snippets",
 }
 
 with open('config.json', 'w') as f:
